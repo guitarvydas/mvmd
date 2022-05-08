0545628e6b7af422b2f67d24d93db4d704c0de06
Paul Tarvydas <guitarvydas@gmail.com>
2022-05-08 16:16:51 -0400

    begin lines()

--- fmt.js
+++ fmt.js
@@ -1,6 +1,35 @@
 exports.pythonify = function (s) {
     return s
-	.replace ("<", "lt_")
-	.replace (">", "gt_")
+	.replace ("<", "LT")
+	.replace (">", "GT")
     ;
+}
+
+var indent = undefined;
+
+exports.setIndent = function (n) { indext = n; }
+exports.ind = function () { indent  += 4; return ""; }
+exports.outd = function () { indent  -= 4; return ""; }
+
+exports.lines = function (s) {
+    console.log ('b lines...');
+    console.log (s);
+    console.log ('e lines...');
+    var lines = s.split ('\n');
+    console.log (lines);
+    var indentedLines = ""
+    lines.forEach (line => {
+	indentedLines += spaces () + line + '\n';
+    });
+    return indentedLines;
+}
+
+function spaces () {
+    var s = "";
+    var n = indent;
+    while (n > 0) {
+	s += " ";
+	n -= 1;
+    }
+    return s;
 }
